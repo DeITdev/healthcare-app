be836a6723b4e40f2025e6b7a16b452a77434200
Danar Ikram T. <89527513+DeITdev@users.noreply.github.com>
2025-04-07 21:31:50 +0700

    implement the otp for admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP VERIFICATION | Passkeymodal */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -14,12 +17,12 @@ export default function Home() {
             height={1000}
             width={1000}
             alt="patient"
-            className="mb-12 h-10 w-fit"
+            className="mb-10 h-10 w-fit"
           />
 
           <PatientForm />
 
-          <div className="text-14-regular mt-20 flex justify-between">
+          <div className="text-14-regular mt-10 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2025 CarePulse
             </p>
